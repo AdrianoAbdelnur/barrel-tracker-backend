e5bd8fe39929eb2ada68a0e12237b166c9e154b8
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-09-01 16:31:08 -0300

    Code verification added

--- src/routes/barrel.js
+++ src/routes/barrel.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { addBarrel, getBarrels, changeStatus, getABarrel } = require('../controllers/barrel');
+const { verifyQrCode } = require('../middleware/barrel');
 const router = express.Router();
 
-router.post('/addBarrel', addBarrel)
+router.post('/addBarrel',verifyQrCode, addBarrel)
 router.get('/getBarrels', getBarrels)
 router.put('/status/:id', changeStatus)
-router.get('/getABarrel/:id', getABarrel)
+router.get('/getABarrel/:id',verifyQrCode,getABarrel)
 
 module.exports= router;
