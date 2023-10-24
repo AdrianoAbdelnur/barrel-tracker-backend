cae75594a0b64b8db3674ce3938cd66bb3f0ecee
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-10-24 14:44:09 -0300

    updateStockByRecipe controller added

--- src/controllers/ingredient.js
+++ src/controllers/ingredient.js
@@ -50,9 +50,29 @@ const updatePricesByFile = async (req, res) => {
             await newIngredient.save();
             return res.status(200).json({message: 'The ingredient was added correctly', newIngredient})
         }
-
     } catch (error) {
-        
+        res.status(error.code || 500).json({message : error.message})
+    }
+}
+
+const updateStockByRecipe = async(req, res) => {
+    try {
+        const {malts, cleanings, hops, yeasts} = req.body;
+        for (const malt of malts) {
+            await Ingredient.findByIdAndUpdate(malt.item._id, {$inc: {stock: -(malt.quantity)}}, {new:true});
+        }
+        for (const hop of hops) {
+            await Ingredient.findByIdAndUpdate(hop.item._id, {$inc: {stock: -(hop.quantity)}}, {new:true});
+        }
+        for (const cleaning of cleanings) {
+            await Ingredient.findByIdAndUpdate(cleaning.item._id, {$inc: {stock: -(cleaning.quantity)}}, {new:true});
+        }
+        for (const yeast of yeasts) {
+            await Ingredient.findByIdAndUpdate(yeast.item._id, {$inc: {stock: -(yeast.quantity)}}, {new:true});
+        }
+        res.status(200).json({message: "Stock updated succesfully"})
+    } catch (error) {
+        res.status(error.code || 500).json({message : error.message})
     }
 }
 
@@ -62,5 +82,6 @@ module.exports= {
     getIngredients,
     updatePrices,
     updatePricesByFile,
-    updateStock
+    updateStock,
+    updateStockByRecipe
 }
--- src/routes/ingredient.js
+++ src/routes/ingredient.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { addNewIngredient, getIngredients, updatePrices, updatePricesByFile, updateStock } = require("../controllers/ingredient");
+const { addNewIngredient, getIngredients, updatePrices, updatePricesByFile, updateStock, updateStockByRecipe } = require("../controllers/ingredient");
 const router = express.Router();
 
 router.post('/newIngredient', addNewIngredient)
@@ -7,5 +7,6 @@ router.get('/getIngredients', getIngredients)
 router.patch('/updatePrices', updatePrices)
 router.patch('/updateStock', updateStock)
 router.patch('/updatePricesByFile', updatePricesByFile)
+router.patch('/updateStockByRecipe', updateStockByRecipe)
 
 module.exports= router;
